2e15590c7693bdf439d8b5c0b9cc2adbba7a0622
byjusmex4 <88998245+byjusmex4@users.noreply.github.com>
2021-08-19 12:20:32 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+var input, heading;
+
+function setup() {
+  
+  createCanvas(300, 200);
+  background(178,255,102);
+  
+  input = createInput();
+  input.position(5, 60);
+
+  heading = createElement('h4', 'Ingrese cualquier letra:');
+  heading.position(5, 20);
+    
+  textAlign(CENTER);
+  textSize(50);
+}
+
+function draw() {
+
+  var value = input.value();
+  switch (value) {
+      
+    case 'a':
+      console.log("Vocal");
+      break;
+      
+    case 'e':
+      console.log("Vocal");
+      break;
+      
+    case 'i':
+      console.log("Vocal");
+      break;
+      
+    case 'o':
+      console.log("Vocal");  
+      break;
+      
+    case 'u':
+      console.log("Vocal");  
+      break;
+      
+    default:
+      console.log("Por favor, ingrese cualquier caracter"); 
+  }
+}
+
+
